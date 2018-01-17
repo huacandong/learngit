93081cac8b468949f2a1191fac1a8585957d603f
Huacd <905088121@qq.com>
2018-01-17 13:48:30 +0800

    add pstr malloc to main.c

--- main.c
+++ main.c
@@ -3,9 +3,16 @@
 int main()
 {
 	int var1;
-	
+	char *pstr = (char *)malloc(1024);	
+
 	var1 = var1 + 2;	
 
 	printf("hello git, this is to test git!\n");
+	
+	memset(pstr, 0, 1024);
+
+	free(pstr);
+	
+
 	return 0;
 }
